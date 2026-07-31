7adc8a73b325201921878cdce26433c6b056e797
vivek kore <vivekkore1122@gmail.com>
2026-07-31 18:58:17 +0530

    feat(auth): implement mock JWT authentication, login flow, token storage, and HTTP interceptor

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,6 +1,6 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {provideHttpClient,withInterceptorsFromDi} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Navbar } from './layout/navbar/navbar';
@@ -16,7 +16,6 @@ import { ProductDetails } from './features/products/product-details/product-deta
 import { AddProduct } from './features/products/add-product/add-product';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AddCategory } from './features/categories/add-category/add-category';
-
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -25,6 +24,9 @@ import { ProductEffects } from './store/effects/product.effects';
 import { MaterialModule } from './shared/material/material.module';
 import { AppCardComponent  } from './shared/components/app-card/app-card';
 import { ProductCardComponent } from './shared/components/product-card/product-card';
+import { Login } from './features/auth/login/login';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 
@@ -44,29 +46,27 @@ import { ProductCardComponent } from './shared/components/product-card/product-c
     AddProduct,
     AddCategory,
     AppCardComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    Login
+    
   ],
-  imports: [
+
+ imports: [
   BrowserModule,
+  BrowserAnimationsModule,
   AppRoutingModule,
   ReactiveFormsModule,
+  HttpClientModule,
   MaterialModule,
   StoreModule.forRoot(reducers),
-
-  EffectsModule.forRoot([
-    ProductEffects
-  ]),
-
+  EffectsModule.forRoot([ProductEffects]),
   StoreDevtoolsModule.instrument({
     maxAge: 25
-  }),
-  ],
+  })
+],
+
   providers: [
     provideBrowserGlobalErrorListeners(),
-
-    provideHttpClient(
-      withInterceptorsFromDi()
-    )
   ],
   bootstrap: [App]
 })
